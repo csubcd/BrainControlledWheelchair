5403025c860b823062686ed0f1bc87baa11edd1d
Jason Gardner <jason.o.gardner@gmail.com>
2017-04-13 19:07:43 -0600

    Fix minor bugs and changed action for backward

--- src/shared/Enumerations.java
+++ src/shared/Enumerations.java
@@ -13,7 +13,7 @@ public class Enumerations {
 	public static MotorState intToMotorState(int state) {
 		switch (state) {
 			case 2: return MotorState.forward;
-			case 4: return MotorState.backward;
+			case 8: return MotorState.backward;
 			case 32: return MotorState.left;
 			case 64: return MotorState.right;
 			default: return MotorState.not_found;
@@ -23,7 +23,7 @@ public class Enumerations {
 	public static int motorStateToInt(MotorState state) {
 		switch(state) {
 		case forward: return 2;
-		case backward: return 4;
+		case backward: return 8;
 		case left: return 32;
 		case right: return 64;
 		default:
